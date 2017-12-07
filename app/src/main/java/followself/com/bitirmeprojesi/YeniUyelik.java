e502cb4317cee89b3682cce24fab93a8d4b8caa6
Kenan Çaltılıoğlu <ce.kenancaltilioglu@gmail.com>
2017-12-07 19:53:27 +0300

    Login kısmı yapıldı

--- app/src/main/java/followself/com/bitirmeprojesi/YeniUyelik.java
+++ app/src/main/java/followself/com/bitirmeprojesi/YeniUyelik.java
@@ -78,7 +78,7 @@ public class YeniUyelik extends AppCompatActivity {
         liste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent listele = new Intent(getApplicationContext(),DatabaseList.class);
+                Intent listele = new Intent(getApplicationContext(),YonetimPaneli.class);
                 startActivity(listele);
             }
         });
