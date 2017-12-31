69e6dbbd8697df08d24fef9ff41a859492deed6e
Kenan Çaltılıoğlu <ce.kenancaltilioglu@gmail.com>
2017-12-31 15:04:25 +0300

    NavBar, Menü kaldı

--- app/src/main/java/followself/com/bitirmeprojesi/Belgeler.java
+++ app/src/main/java/followself/com/bitirmeprojesi/Belgeler.java
@@ -94,11 +94,9 @@ public class Belgeler extends AppCompatActivity {
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Database db = new Database(Belgeler.this);
-                String ad = et1.getText().toString();
-                byte[] resim = db.resimGoster(id,ad);
-                Bitmap bmp = BitmapFactory.decodeByteArray(resim,0,resim.length);
-                iv1.setImageBitmap(bmp);
+                Uri link = Uri.parse("https://accounts.google.com/signin/v2/sl/pwd?service=wise&passive=true&continue=http%3A%2F%2Fdrive.google.com%2F%3Futm_source%3Dtr&utm_medium=button&utm_campaign=web&utm_content=gotodrive&usp=gtd&ltmpl=drive&urp=https%3A%2F%2Fwww.google.com.tr%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+                Intent intent = new Intent(Intent.ACTION_VIEW,link);
+                startActivity(intent);
             }
         });
 
